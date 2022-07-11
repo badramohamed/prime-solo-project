25b9edfca88129e5bbf145a38efce317154b5688
Badra Mohamed <moha1150@umn.edu>
2022-07-10 20:17:55 -0500

    adding alerts to buttons

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -3,47 +3,60 @@ import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
+import swal from "sweetalert";
 
+function Book({ books }) {
+  const dispatch = useDispatch();
 
+  const handleComplete = () => {
+    console.log(books.id);
+    dispatch({
+      type: "FETCH_COMPLETED_BOOKS",
+      payload: books.id,
+    });
+    swal({
+      title: "Good job!",
+      text: "hope you enjoyed the book!",
+      icon: "success",
+    });
 
-function Book ({books}){
-     const dispatch = useDispatch();
+    // history.push('/Details');
+    console.log(">>.>>>>>>>", books);
+  };
 
-    const handleComplete = ()=>{
-        console.log(books.id);
-        dispatch({
-            type: 'FETCH_COMPLETED_BOOKS',
-            payload: books.id,
-            
-        })
-        // history.push('/Details');
-        console.log('>>.>>>>>>>',books)
-        
-    }
+  const handleDelete = () => {
+    dispatch({
+      type: "DELETE_DB_BOOKS",
+      payload: books.id,
+    });
+    swal({
+      title: "DELETE",
+      text: "Are you sure?",
+      icon: "warning",
+      buttons: true,
+      dangerMode: true,
+    }).then((willDelete) => {
+      if (willDelete) {
+        swal("Your book has been deleted! Thank you! ", {
+          icon: "success",
+        });
+      } else {
+        swal("No edits have been made");
+      }
+    });
+  };
 
-    const handleDelete = ()=>{
-        dispatch({
-            type:'DELETE_DB_BOOKS',
-            payload: books.id
-        })
-
-
-    }
-    
-    return (
-        <>
-         <div >
-
-                <h3>{books.title}</h3>
-                <h3>{books.author}</h3>
-                <h3>{books.description}</h3>
-                <img src={books.cover} alt={books.title}/>
-                <button onClick={handleDelete}> delete</button>
-                <button onClick={handleComplete}> complete</button>
-
-
-                </div>
-        </>
-    )
+  return (
+    <>
+      <div>
+        <h3>{books.title}</h3>
+        <h3>{books.author}</h3>
+        <h3>{books.description}</h3>
+        <img src={books.cover} alt={books.title} />
+        <button onClick={handleDelete}> delete</button>
+        <button onClick={handleComplete}> complete</button>
+      </div>
+    </>
+  );
 }
 export default Book;
